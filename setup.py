0cb9d2c06d1b6850459282cc5347ed9f9ab5ab1a
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 11:52:04 +0200

    Releasing version 1

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-validate-model-attribute-assignment',
+    version=1,
     packages=(
         'django_validate_model_attribute_assignment',
     )
