15ff616c1671363deab0a517e4c8f62dd08157ab
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-14 15:52:39 +0100

    Releasing version 2.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-validate-model-attribute-assignment',
 
     url="https://chris-lamb.co.uk/projects/django-validate-model-attribute-assignment",
-    version='2',
+    version='2.0.1',
     description="Prevent typos and other errors when assigning attributes to Django model instances",
 
     author="Chris Lamb",
